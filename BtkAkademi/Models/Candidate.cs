2b6b3cf0f9457cc94faeba557cf8ba5c4a503857
Enes Esen <g.enesesen@gmail.com>
2023-06-01 15:51:01 +0300

    btkakademi

---
+++ BtkAkademi/Models/Candidate.cs
@@ -0,0 +1,18 @@
+namespace BtkAkademi.Models
+{
+    public class Candidate
+    {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? Selectedcourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+        //Kayıt anını kaydetmek için bir constructer kullarak o aki tarih değerini veririz.
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
